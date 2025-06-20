df50ab0214d6cbf165c8563ce8ac4d72b4ac0174
drizz27 <johngalang1000@gmail.com>
2025-06-20 10:37:20 +0800

    Added new txtfile

---
+++ pages/signin.vue
@@ -0,0 +1,63 @@
+// ...existing code...
+<template>
+  <v-form>
+    <v-container
+      fluid
+      class="d-flex justify-center align-center"
+      style="min-height: 100vh;"
+    >
+      <v-row justify="center" align="center" class="w-100">
+        <v-col cols="12" md="5" class="d-flex flex-column align-center">
+          <v-card class="pa-6 d-flex flex-column align-center" elevation="2" style="border-radius: 5px; width: 65%;">
+            <div class="d-flex justify-center align-center mb-4">
+              <h1 class="text-h5 font-weight-medium text-center">Login</h1>
+            </div>
+            <v-text-field
+              prepend-inner-icon="mdi-email"
+              v-model="email"
+              label="Email Address"
+              solo-inverted
+              dense
+              rounded
+              class="mb-3"
+              style="text-align: center;"
+            />
+            <v-text-field
+              prepend-inner-icon="mdi-lock"
+              v-model="password"
+              label="Password"
+              type="password"
+              solo-inverted
+              dense
+              rounded
+              class="mb-4"
+              style="text-align: center;"
+            />
+            <div class="d-flex justify-center">
+              <v-btn
+                color="primary"
+                @click="login"
+              >
+                Login
+              </v-btn>
+
+                <v-btn
+                class="ml-5"
+                color="secondary"
+                @click="login"
+              >
+                Sign in Using google
+              </v-btn>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  layout: "auth",
+}
+</script>
